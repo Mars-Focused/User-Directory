94712c27780c093bec866e51aa6f8d35283e8dfd
Mars_Clarke <marsthecoder@outlook.com>
2020-11-19 15:27:43 -0700

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header(){
+    return <header className='header'>
+        <h1>Mars's REDACTED tracker</h1>
+    </header>
+}
+
+export default Header
---
+++ src/Components/UserContainer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+// import UserList from './UserList'
+// import UserDetail from './UserDetail'
+// import User from './User'
+
+class UserContainer extends Component {
+    constructor(){
+        super()
+    }
+
+    render(){
+        return( <div className='user-container'>
+            <h1>This Is the REDACTED Container</h1>
+        </div>)
+    }
+}
+
+export default UserContainer
