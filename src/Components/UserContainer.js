73345999d0571c9e7b15428e2f016c94594457e0
Mars_Clarke <marsthecoder@outlook.com>
2020-11-19 23:36:49 -0700

    crying and coding

--- src/Components/UserContainer.js
+++ src/Components/UserContainer.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react'
 import UserDetail from './UserDetail'
+// import users from './data'
 // import UserList from './UserList'
 // import User from './User'
 
 class UserContainer extends Component {
     constructor(){
         super()
+
+        this.state = {
+            currentIndex: 0
+        }
+        // this.nextIndex
     }
+
+    // nextIndex(currentIndex) => {
 
     render(){
         return( <div className='user-container'>
-            <h1>This Is the User Container</h1>
+            <h1>this is User Container</h1>
             <UserDetail/>
             {/* <UserList/> */}
         </div>)
