fb9bb5b8a31a826fd5a90de08220aa4d9b929a82
Mars_Clarke <marsthecoder@outlook.com>
2021-02-03 18:44:58 -0700

    working to finish up

---
+++ src/Components/UserBox.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class UserBox extends Component {
+  constructor() {
+    super();
+    // this.state = {}
+  }
+  render() {
+    return (
+      <div className="user-detail">
+        <h1>This is UserDetail</h1>
+      </div>
+    );
+  }
+}
+
+export default UserBox;
--- src/Components/UserContainer.js
+++ src/Components/UserContainer.js
@@ -1,28 +1,30 @@
-import React, {Component} from 'react'
-import UserDetail from './UserDetail'
+import React, { Component } from "react";
+import UserBox from "./UserBox";
 // import users from './data'
 // import UserList from './UserList'
 // import User from './User'
 
 class UserContainer extends Component {
-    constructor(){
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            currentIndex: 0
-        }
-        // this.nextIndex
-    }
+    this.state = {
+      currentIndex: 0,
+    };
+    // this.nextIndex
+  }
 
-    // nextIndex(currentIndex) => {
+  // nextIndex(currentIndex) => {
 
-    render(){
-        return( <div className='user-container'>
-            <h1>this is User Container</h1>
-            <UserDetail/>
-            {/* <UserList/> */}
-        </div>)
-    }
+  render() {
+    return (
+      <div className="user-container">
+        <h1>this is User Container</h1>
+        <UserBox />
+        {/* <UserList/> */}
+      </div>
+    );
+  }
 }
 
-export default UserContainer
+export default UserContainer;
